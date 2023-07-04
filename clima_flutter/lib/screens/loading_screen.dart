81eea131ca5797566c06c077b03c5e7469a10e7f
Aniket Goyal <aniketgoyal291@gmail.com>
2023-07-04 15:15:52 +0530

    Commiting to save

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima_flutter/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,10 +9,24 @@ class LoadingScreen extends StatefulWidget {
 
 void getLocation() async {
   Location location = Location();
+
   await location.getCurrentLocation();
+
   print(location.latitude);
   print(location.longitude);
-  print(location.longitude);
+}
+
+void getData() async {
+  http.Response response = await http.get(
+    Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1'),
+  );
+  if (response.statusCode == 200) {
+    String data = response.body;
+    print(data);
+  } else {
+    print(response.statusCode);
+  }
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
@@ -30,6 +45,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // } catch (e) {
     //   print(e);
     // }
+    getData();
     return const Scaffold();
   }
 }
